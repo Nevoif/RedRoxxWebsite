d53ceb66a7c00c0b0fa97e0def088edc09afb9fe
nevoif <nevoif@hotmail.com>
2026-08-01 21:44:40 +0300

    Rebuild docs for GitHub Pages

--- obj/Debug/net10.0/RedRoxxWebsite.AssemblyInfo.cs
+++ obj/Debug/net10.0/RedRoxxWebsite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RedRoxxWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d954ccdab2c22892c272a68cd6c3b38c628fb13e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd4259765616ced500867b78a5d1a064fc768a0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("RedRoxxWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RedRoxxWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
